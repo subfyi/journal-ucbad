7b00a5507dc90a59865bad0a57cfd45e2c650277
akinayturan <akinayturan@gmail.com>
2020-08-09 23:27:40 +0300

    edt

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,14 @@
 import Layout from "../components/Layout";
 import Topbar from "../components/Topbar";
 import NavOne from "../components/NavOne";
-import SliderOne from "../components/SliderOne";
 import Footer from "../components/Footer";
 import AboutTwo from "../components/AboutTwo";
-import CourseOne from "../components/CourseOne";
-import VideoTwo from "../components/VideoTwo";
-import CountdownKipso from "../components/CountdownKipso";
-import CourseCatOne from "../components/CourseCatOne";
-import CallToActionThree from "../components/CallToActionThree";
-import BrandsTwo from "../components/BrandsTwo";
-import BlogTwo from "../components/BlogTwo";
-import CallToActionFour from "../components/CallToActionFour";
-import SubscribeOne from "../components/SubscribeOne";
 import AboutOne from "../components/AboutOne";
 
 
 const HomePage = () => (
 
-    <Layout pageTitle="Kipso - React Next Online Education Learning & LMS Template">
+    <Layout pageTitle="Ulusal Çevre Bilimleri Araştırma Dergisi | UCBAD">
         <Topbar/>
         <NavOne/>
         <AboutOne/>
