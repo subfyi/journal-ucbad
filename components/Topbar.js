8a566c36b7b2d397497ea9f0165dcbb1505cc8fd
akinayturan <akinayturan@gmail.com>
2020-08-12 20:32:53 +0300

    edt

--- components/Topbar.js
+++ components/Topbar.js
@@ -6,11 +6,11 @@ class Topbar extends Component {
             <div className="topbar-one">
                 <div className="container">
                     <div className="topbar-one__left">
-                        <a target="_blank" href="https://dergipark.org.tr/tr/pub/ucbad">ULUSAL ÇEVRE BİLİMLERİ ARAŞTIRMA DERGİSİ</a>
+                        <a href="https://dergipark.org.tr/tr/pub/ucbad">ULUSAL ÇEVRE BİLİMLERİ ARAŞTIRMA DERGİSİ</a>
                         <a target="_blank" href="https://portal.issn.org/resource/issn/2636-7483">ISSN-2636-7483</a>
                     </div>
                     <div className="topbar-one__right">
-                        <a target="_blank" href="https://dergipark.org.tr/tr/journal/2662/submission/step/manuscript/new">Makale Gönder</a>
+                        <a href="https://dergipark.org.tr/tr/journal/2662/submission/step/manuscript/new">Makale Gönder</a>
                     </div>
                 </div>
             </div>
