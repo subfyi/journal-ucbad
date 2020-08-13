6e864eda790c26e04ed32478d05c23b0f083e94b
akinayturan <akinayturan@gmail.com>
2020-08-13 07:24:03 +0300

    edt

--- components/Contact.js
+++ components/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
                                 <div className="contact-info-one__icon"><i className="kipso-icon-contact"></i>
                                 </div>
                                 <h2 className="contact-info-one__title">Yayıncı</h2>
-                                <p className="contact-info-one__text">Zeynep Cansy Ayturan, Konya, Türkiye <br /> ucbad@ucbad.com</p>
+                                <p className="contact-info-one__text">Zeynep Cansu Ayturan, Konya, Türkiye <br /> ucbad@ucbad.com</p>
                             </div>
                         </div>
                         <div className="col-lg-6">
