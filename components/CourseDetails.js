bdf2c7bf94a11c24468f6ed0e2b59292c9696862
akinayturan <akinayturan@gmail.com>
2020-09-06 20:56:23 +0300

    edt 3

--- components/CourseDetails.js
+++ components/CourseDetails.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import moment from "moment";
+import Link from "next/link";
 
 const CourseDetails = (props) => {
-    const {articles, volume, issue, article} = props;
+    const {articles, volume, volumes, issue, article} = props;
 
     return (
         <section className="course-details">
@@ -110,6 +111,38 @@ const CourseDetails = (props) => {
                                 Sayfa: <span>  {articles.first_page}-{articles.last_page}</span>
                             </a>
                         </div>
+
+                        <>
+                            <div className="course-details__price">
+
+                                <div className=" text-center align-items-center">
+
+                                    <Link href={"/doc/ucbad-" + moment(articles.pubdate).format("YY") + "-0" + issue + "-00-00.pdf"}><a target="_blank">
+                                        <img className="w-100 mb-3" src={require("../public/doc/ucbad-" + moment(articles.pubdate).format("YY") + "-0" + issue + "-00-00.png")}
+                                             alt="volume image"/>
+                                    </a></Link>
+
+                                    <Link href={"/doc/ucbad-" + moment(articles.pubdate).format("YY") + "-0" + issue + "-00-00.pdf"}>
+                                        <a target="_blank" className="btn btn-outline-dark text-secondary col-md-12 mb-3">
+                                            COVER
+                                        </a></Link>
+
+                                    <Link href={"/doc/ucbad-" + moment(articles.pubdate).format("YY") + "-00-00-01.pdf"}>
+                                        <a target="_blank" className="btn btn-outline-dark text-secondary col-md-12 mb-3">
+                                            EDITORS
+                                        </a></Link>
+                                    <Link href={"/doc/ucbad-" + moment(articles.pubdate).format("YY") + "-00-00-02.pdf"}>
+                                        <a target="_blank" className="btn btn-outline-dark text-secondary col-md-12 mb-3">
+                                            ABOUT
+                                        </a></Link>
+                                    <Link href={"/doc/ucbad-" + moment(articles.pubdate).format("YY") + "-0" + issue + "-00-03.pdf"}>
+                                        <a target="_blank" className="btn btn-outline-dark text-secondary col-md-12 mb-3">
+                                            CONTENTS
+                                        </a></Link>
+
+                                </div>
+                            </div>
+                        </>
 
                     </div>
                     <div className="col-lg-12">
