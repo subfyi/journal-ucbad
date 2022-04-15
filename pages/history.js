22e553a80f166474544ca6bff763f2c5ea2ba8ef
Akın Ayturan <akinayturan@gmail.com>
2022-04-15 13:17:25 +0300

    edt 2.2

--- pages/history.js
+++ pages/history.js
@@ -30,18 +30,335 @@ const TeachersPage = () => {
                   </tr>
                   </tbody>
                 </table>
-                <p><strong>Infos</strong></p>
+                <p><strong>Diğer Bilgiler</strong></p>
                 <table className="tg" border="0">
                   <tbody>
                   <tr>
-                    <td>2018</td>
-                    <td><em>Kurucu</em>: Zeynep Cansu Ayturan
-                      <br />
-                      <em>Editor-in-Chief </em>: Prof. Dr. Sukru Dursun
+                    <td><b>2018</b>  (Cilt 1 Sayı 1, Cilt 1 Sayı 2, Cilt 1 Sayı 3, Cilt 1 Sayı 4)</td>
+                  </tr>
+                  <tr>
+                    <td>Zeynep Cansu Ayturan<br/>
+
                     </td>
                   </tr>
+                  <tr>
+                    <td>
+                      <em>Baş Editör </em>: Prof. Dr. Sukru Dursun<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Yardımcı Editör </em>: Ms. Zeynep Cansu Ayturan<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Diğer Editörler </em>: <br/>
+                      Dr. Bahtiyar Öztürk
+                      <br/>
+                      Dr. Bilal Tunçsiper
+                      <br/>
+                      Dr. Çiğdem Çiftçi
+                      <br/>
+                      Dr. Delia Teresa Sponza
+                      <br/>
+                      Dr. Durmuş Kaya
+                      <br/>
+                      Dr. Fatma Çanka Kılıç
+                      <br/>
+                      Dr. Filiz Nuran Acar
+                      <br/>
+                      Dr. Güray Çelik Salihoğlu
+                      <br/>
+                      Dr. Hüseyin Toros
+                      <br/>
+                      Dr. Kübra Yazıcı
+                      <br/>
+                      Dr. Mehmet Emin Argun
+                      <br/>
+                      Dr. Mehmet Kılıç
+                      <br/>
+                      Dr. Mustafa Işık
+                      <br/>
+                      Dr. Nilgün Balkaya
+                      <br/>
+                      Dr. Nevim Genç
+                      <br/>
+                      Dr. Osman Nuri Ağdağ
+                      <br/>
+                      Dr. Simge Bardak Denerel
+                      <br/>
+                      Dr. Yağmur Uysal
+                    </td>
+                  </tr>
+
+
                   </tbody>
                 </table>
+
+                <table className="tg" border="0">
+                  <tbody>
+                  <tr>
+                    <td><b>2019</b>  (Cilt 2 Sayı 1, Cilt 2 Sayı 2, Cilt 2 Sayı 3, Cilt 2 Sayı 4)</td>
+                  </tr>
+                  <tr>
+                    <td>Zeynep Cansu Ayturan<br/>
+
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Baş Editör </em>: Prof. Dr. Sukru Dursun<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Yardımcı Editör </em>: Ms. Zeynep Cansu Ayturan<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Diğer Editörler </em>: <br/>
+                      Dr. Bahtiyar Öztürk
+                      <br/>
+                      Dr. Bilal Tunçsiper
+                      <br/>
+                      Dr. Çiğdem Çiftçi
+                      <br/>
+                      Dr. Delia Teresa Sponza
+                      <br/>
+                      Dr. Durmuş Kaya
+                      <br/>
+                      Dr. Fatma Çanka Kılıç
+                      <br/>
+                      Dr. Filiz Nuran Acar
+                      <br/>
+                      Dr. Güray Çelik Salihoğlu
+                      <br/>
+                      Dr. Hüseyin Toros
+                      <br/>
+                      Dr. Kübra Yazıcı
+                      <br/>
+                      Dr. Mehmet Emin Argun
+                      <br/>
+                      Dr. Mehmet Kılıç
+                      <br/>
+                      Dr. Mustafa Işık
+                      <br/>
+                      Dr. Nilgün Balkaya
+                      <br/>
+                      Dr. Nevim Genç
+                      <br/>
+                      Dr. Osman Nuri Ağdağ
+                      <br/>
+                      Dr. Simge Bardak Denerel
+                      <br/>
+                      Dr. Yağmur Uysal
+                    </td>
+                  </tr>
+
+
+                  </tbody>
+                </table>
+
+
+                <table className="tg" border="0">
+                  <tbody>
+                  <tr>
+                    <td><b>2020</b>  (Cilt 3 Sayı 1, Cilt 3 Sayı 2, Cilt 3 Sayı 3, Cilt 3 Sayı 4)</td>
+                  </tr>
+                  <tr>
+                    <td>Zeynep Cansu Ayturan<br/>
+
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Baş Editör </em>: Prof. Dr. Sukru Dursun<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Yardımcı Editör </em>: Ms. Zeynep Cansu Ayturan<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Diğer Editörler </em>: <br/>
+                      Dr. Bahtiyar Öztürk
+                      <br/>
+                      Dr. Bilal Tunçsiper
+                      <br/>
+                      Dr. Çiğdem Çiftçi
+                      <br/>
+                      Dr. Delia Teresa Sponza
+                      <br/>
+                      Dr. Durmuş Kaya
+                      <br/>
+                      Dr. Fatma Çanka Kılıç
+                      <br/>
+                      Dr. Filiz Nuran Acar
+                      <br/>
+                      Dr. Güray Çelik Salihoğlu
+                      <br/>
+                      Dr. Hüseyin Toros
+                      <br/>
+                      Dr. Kübra Yazıcı
+                      <br/>
+                      Dr. Mehmet Emin Argun
+                      <br/>
+                      Dr. Mehmet Kılıç
+                      <br/>
+                      Dr. Mustafa Işık
+                      <br/>
+                      Dr. Nilgün Balkaya
+                      <br/>
+                      Dr. Nevim Genç
+                      <br/>
+                      Dr. Osman Nuri Ağdağ
+                      <br/>
+                      Dr. Simge Bardak Denerel
+                      <br/>
+                      Dr. Yağmur Uysal
+                    </td>
+                  </tr>
+
+
+                  </tbody>
+                </table>
+
+
+                <table className="tg" border="0">
+                  <tbody>
+                  <tr>
+                    <td><b>2021</b>  (Cilt 4 Sayı 1, Cilt 4 Sayı 2)</td>
+                  </tr>
+                  <tr>
+                    <td>Zeynep Cansu Ayturan<br/>
+
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Baş Editör </em>: Prof. Dr. Sukru Dursun<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Yardımcı Editör </em>: Ms. Zeynep Cansu Ayturan<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Diğer Editörler </em>: <br/>
+                      Dr. Bahtiyar Öztürk
+                      <br/>
+                      Dr. Bilal Tunçsiper
+                      <br/>
+                      Dr. Çiğdem Çiftçi
+                      <br/>
+                      Dr. Delia Teresa Sponza
+                      <br/>
+                      Dr. Durmuş Kaya
+                      <br/>
+                      Dr. Fatma Çanka Kılıç
+                      <br/>
+                      Dr. Filiz Nuran Acar
+                      <br/>
+                      Dr. Güray Çelik Salihoğlu
+                      <br/>
+                      Dr. Hüseyin Toros
+                      <br/>
+                      Dr. Kübra Yazıcı
+                      <br/>
+                      Dr. Mehmet Emin Argun
+                      <br/>
+                      Dr. Mehmet Kılıç
+                      <br/>
+                      Dr. Mustafa Işık
+                      <br/>
+                      Dr. Nilgün Balkaya
+                      <br/>
+                      Dr. Nevim Genç
+                      <br/>
+                      Dr. Osman Nuri Ağdağ
+                      <br/>
+                      Dr. Simge Bardak Denerel
+                      <br/>
+                      Dr. Yağmur Uysal
+                    </td>
+                  </tr>
+
+
+                  </tbody>
+                </table>
+
+
+                <table className="tg" border="0">
+                  <tbody>
+                  <tr>
+                    <td><b>2022</b>  (Cilt 5 Sayı 1)</td>
+                  </tr>
+                  <tr>
+                    <td>Zeynep Cansu Ayturan<br/>
+
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Baş Editör </em>: Prof. Dr. Sukru Dursun<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Yardımcı Editör </em>: Dr. Zeynep Cansu Ayturan<br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <em>Diğer Editörler </em>: <br/>
+                      Dr. Bahtiyar Öztürk
+                      <br/>
+                      Dr. Bilal Tunçsiper
+                      <br/>
+                      Dr. Çiğdem Çiftçi
+                      <br/>
+                      Dr. Delia Teresa Sponza
+                      <br/>
+                      Dr. Durmuş Kaya
+                      <br/>
+                      Dr. Fatma Çanka Kılıç
+                      <br/>
+                      Dr. Filiz Nuran Acar
+                      <br/>
+                      Dr. Güray Çelik Salihoğlu
+                      <br/>
+                      Dr. Hüseyin Toros
+                      <br/>
+                      Dr. Kübra Yazıcı
+                      <br/>
+                      Dr. Mehmet Emin Argun
+                      <br/>
+                      Dr. Mehmet Kılıç
+                      <br/>
+                      Dr. Mustafa Işık
+                      <br/>
+                      Dr. Nilgün Balkaya
+                      <br/>
+                      Dr. Nevim Genç
+                      <br/>
+                      Dr. Osman Nuri Ağdağ
+                      <br/>
+                      Dr. Simge Bardak Denerel
+                      <br/>
+                      Dr. Yağmur Uysal
+                    </td>
+                  </tr>
+
+
+                  </tbody>
+                </table>
+
               </div>
             </div>
           </div>
