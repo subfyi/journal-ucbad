878aa8f7ce6147f4aab787ba175fb47bd21b1650
akinayturan <akinayturan@gmail.com>
2023-10-30 10:03:10 +0300

    Refactor contact page and modify metadata
    Contact page and its metadata underwent revisions to better suit the needs of the non-English speaking audience. Additionally, code was refactored to streamline the structure. This change would enhance the user experience for a wider international audience and improve the

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,9 @@
-
-import { PageHeader } from '@/layout/Breadcrumb'
+import {PageHeader} from '@/layout/Breadcrumb'
 import Contact from "@/components/Contact";
 import NavOne from "@/components/NavOne";
 import ContactForm from "@/components/ContactForm";
 
-const metaTitle = 'Contact'
+const metaTitle = 'İletişim'
 export const metadata = {
     title: metaTitle + ' | UCBAD',
     description: 'İletişim için buradan bize ulaşabilirsiniz.',
@@ -15,9 +14,9 @@ export const metadata = {
 }
 export default function Page() {
     return <>
-            <NavOne />
-            <PageHeader title="Contact" />
-            <Contact />
-            <ContactForm />
+        <NavOne/>
+        <PageHeader title={metaTitle}/>
+        <Contact/>
+        <ContactForm/>
     </>;
 }
