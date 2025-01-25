c40d632cb2fc335c675c3a443505efd3b2996ef6
akinayturan <akinayturan@gmail.com>
2025-01-25 19:49:59 +0300

    edt 1.6

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='tr'>
     <head>
       <meta charSet='UTF-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
